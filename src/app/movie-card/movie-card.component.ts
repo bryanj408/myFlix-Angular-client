4e5aae62dfc5f23706bb942da23bd93e162a9777
bryanj408 <bryan.arndt52@gmail.com>
2022-09-07 09:24:03 -0700

    added movie-card .component

---
+++ src/app/movie-card/movie-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service'
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.scss']
+})
+export class MovieCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
